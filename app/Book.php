31a349a0d7476ac9d9144d8d63dd2cbbfdb8712a
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-08-24 16:57:06 +0900

    ロジックをBookモデルに移動しました。validationをコントローラに追加しました

--- app/Book.php
+++ app/Book.php
@@ -8,10 +8,18 @@ class Book extends Model
 {
     protected $fillable=["user_id","isbn","name"];
 
-    public static function getName ($isbn)
+    public static function validateISBN($isbn)
     {
         //ISBNの値のバリデーション
-        if(!is_numeric($isbn) || mb_strlen($isbn)!==13){
+        if (!is_numeric($isbn) || mb_strlen($isbn) !== 13) {
+            return false;
+        }
+    }
+
+    public static function getName($isbn)
+    {
+        //ISBNの値のバリデーション
+        if(!is_numeric($isbn) || mb_strlen($isbn) !== 13){
             return false;
         }
 
@@ -20,5 +28,43 @@ class Book extends Model
         $xml_part = (string)$xml->records->record->recordData;
         $name = substr($xml_part, strpos($xml_part, "<dc:title>") + 10, strpos($xml_part, "</dc:title>") - (strpos($xml_part, "<dc:title>") + 10));
         return $name;
+    }
+
+    public static function getState($isbn, $campus)
+    {
+        //図書館のURLです
+        $url = "http://opac.lib.meiji.ac.jp/webopac/ctlsrh.do";
+
+        switch ($campus) {
+            case "nakano": $campus="MN"; break;
+            case "ikuta": $campus="MS"; break;
+            case "surugadai": $campus="MH"; break;
+            case "izumi": $campus="MW"; break;
+            case "all": $campus="all"; break;
+            default: $campus="MN"; break;
+        }
+
+        $postdata = array("holar" => $campus, "isbn_issn" => $isbn);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+        $html = curl_exec($ch);
+        curl_close($ch);
+
+        //所蔵がある場合
+        if(strpos($html,"所蔵はありません")===false && strpos($html,"指定された条件に該当する資料がありませんでした")===false){
+            //</strong>があるので10文字分戻しています
+            $start_pos=mb_strpos($html,"件の所蔵があります")-10;
+            $book_num=mb_substr($html,$start_pos,1);//本の数が手に入る
+            preg_match_all("/貸出中/",$html,$match);
+            if($book_num==count($match[0])){
+                return "貸出中";
+            }else if($book_num>count($match[0])){
+                return "OK";
+            }
+        }else{
+            return "所蔵無し";//所蔵が無い場合
+        }
     }
 };
--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -19,9 +19,14 @@ class BooksController extends Controller
     //isbnのクエリを取得、13または10文字だったら登録する→validation？
     public function register(Request $request)
     {
+        $this->validate($request, [
+            'isbn' => 'required|size:13'
+        ]);
+
         $isbn = $request->input('isbn');
         $name = Book::getName($isbn);//名前を取得する
 
+        //名前が取得できなかったらfalse
         if(!$name){
             return false;
         }
@@ -37,6 +42,10 @@ class BooksController extends Controller
 
     public function delete(Request $request)
     {
+        $this->validate($request, [
+            'id' => 'required'
+        ]);
+
         $id=intval($request->input("id"));
         $book=Book::find($id);
         $book->delete();
@@ -57,41 +66,12 @@ class BooksController extends Controller
         return $books;
     }
 
-    public function state(Request $request){
-        $isbn=$request->input("isbn");
-        $campus=$request->input("campus");
-        if(empty($isbn) || strlen($isbn)!==13){
-            return false;
-        }
-        $url="http://opac.lib.meiji.ac.jp/webopac/ctlsrh.do";
-        switch($campus){
-            case "nakano":$campus="MN";break;
-            case "ikuta":$campus="MS";break;
-            case "surugadai":$campus="MH";break;
-            case "izumi":$campus="MW";break;
-            case "all":$campus="all";break;
-            default: $campus="MN";break;
-        }
-        $postdata=array("holar"=>$campus,"isbn_issn"=>$isbn);
-        $ch=curl_init($url);
-        curl_setopt($ch,CURLOPT_POST,true);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($ch,CURLOPT_POSTFIELDS,$postdata);
-        $html=curl_exec($ch);
-        curl_close($ch);
+    public function state(Request $request)
+    {
+        $this->validate($request, [
+            'isbn' => 'required|size:13'
+        ]);
 
-        //所蔵がある場合
-        if(strpos($html,"所蔵はありません")===false && strpos($html,"指定された条件に該当する資料がありませんでした")===false){
-            $start_pos=mb_strpos($html,"件の所蔵があります")-10;//</strong>があるので10文字分戻しています
-            $book_num=mb_substr($html,$start_pos,1);//本の数が手に入る
-            preg_match_all("/貸出中/",$html,$match);
-            if($book_num==count($match[0])){
-                return "貸出中";
-            }else if($book_num>count($match[0])){
-                return "OK";
-            }
-        }else{//所蔵が無い場合
-            return "所蔵無し";
-        }
+        return Book::getState($request->input("isbn"), $request->input("campus"));
     }
 }
