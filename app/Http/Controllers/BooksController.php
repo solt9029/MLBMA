1ff749ac6ef6fad8de504894f8bfbc2196e70705
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-08-27 14:27:23 +0900

    booksControllerにauthミドルウェアを追加しました（なぜかこれのせいで前はエラーが起こっていたような気がするので、注意する）

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -12,10 +12,10 @@ use App\Http\Controllers\Controller;
 
 class BooksController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     //isbnのクエリを取得、13または10文字だったら登録する→validation？
     public function register(Request $request)
