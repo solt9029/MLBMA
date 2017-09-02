848f47fe13e03e207223c13abf90475ab433d496
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-09-02 10:35:28 +0900

    アイコン画像周りが整理されました

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,12 +5,13 @@
     <title>MLBMA</title>
     <link rel="shortcut icon" href="/img/favicon.jpg"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="css/app.css"/>
+    <link rel="stylesheet" href="/css/app.css"/>
+    <link rel="apple-touch-icon" href="/img/home.jpg" />
 </head>
 <body>
     <div id="app">
         <a href="/auth/twitter" role="button">Login with Twitter</a>
     </div>
-    <script src="js/app.js"></script>
+    <script src="/js/app.js"></script>
 </body>
 </html>
