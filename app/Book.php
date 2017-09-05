7b1ae3815353ea3aa95d5a1f3b045197962d78bd
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-09-06 00:02:26 +0900

    空けました

--- app/Book.php
+++ app/Book.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    protected $fillable=["user_id", "isbn", "name", 'author', 'published', 'description', 'page', 'thumbnail'];
+    protected $fillable = ["user_id", "isbn", "name", 'author', 'published', 'description', 'page', 'thumbnail'];
 
     public function user()
     {
