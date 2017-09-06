862704501c21f04ad18dd89b7f16beb7907d90ef
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-09-06 20:32:17 +0900

    navbarをインポートしました

--- resources/assets/js/components/LoginPage.vue
+++ resources/assets/js/components/LoginPage.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
+        <app-navbar></app-navbar>
         This is a login page!!
         <a href="/auth/twitter" role="button">Login with Twitter</a>
     </div>
 </template>
 
 <script>
+import AppNavbar from './AppNavbar'
 export default {
-    name: 'login-page'
+    name: 'login-page',
+    components: {
+        AppNavbar
+    }
 }
 </script>
 
