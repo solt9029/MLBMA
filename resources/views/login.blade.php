56bcdbc1d2dbc6f2821443615c8e7d5b5e8a4e41
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-09-15 17:28:45 +0900

    metaタグを設定してツイッターカードに対応するようにしました

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,6 +8,13 @@
     <link rel="stylesheet" href="/css/app.css">
     <link rel="apple-touch-icon" href="/img/home.jpg" />
     <link rel="stylesheet" href="/css/login.css">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="MLBMA">
+    <meta name="twitter:description" content="Meiji Library Books Management Application.明治大学図書館（中野キャンパス）の書籍の貸出状況を確認できる書籍管理アプリケーション。">
+    <meta name="twitter:image" content="http://mlbma.solt9029.com/img/background.jpg">
+    <meta name="twitter:url" content="http://mlbma.solt9029.com">
+    <meta name="twitter:site" content="@solt9029">
+    <meta name="twitter:creator" content="@solt9029">
 </head>
 <body>
     <div id="app" class="section">
